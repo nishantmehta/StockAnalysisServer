a6cdc00f2acaff3f598e6373bd899ec92a3e6d3e
Mayank <mayank.03.12@gmail.com>
2014-04-13 19:02:17 -0400

    mayank - min max query

--- QueryFunctions.py
+++ QueryFunctions.py
@@ -1,3 +1,5 @@
+import heapq
+
 __author__ = 'nishantmehta.n'
 
 
@@ -8,4 +10,39 @@ class queries():
         print "new query request"
         for i in arg.stockHash['goog']:
             print i
+
+    def query(self,coName,obj,n,):
+        # coName : name of the company
+        # obj : obj thread passed
+        # n   : TOP n stocks variable
+
+        print "Top n max and min"
+        #a heapq to hold max 10 values
+        maxHeap = []
+        # a min queue to hold min 10 values
+        minHeap = []
+        # Min Max algorithm
+        stockList = obj.stockHash[coName]
+        j = 0
+        for i in stockList:
+            if j < n:
+              heapq.heappush(maxHeap,stockList[i])
+              heapq.heappush(minHeap,(-1)*stockList[i])
+              j += 1
+            else:
+                if maxHeap[0] < stockList[i]:
+                    heapq.heapreplace(maxHeap,stockList[i])
+                if minHeap[0] < (-1 * stockList[i]):
+                    heapq.heapreplace(minHeap,(-1)*stockList[i])
+
+        #print the max 10 values
+        for i in maxHeap:
+            print(maxHeap[i],end='\n')
+        #print the min 10 values
+        for i in minHeap:
+            print(minHeap[i],end='\n')
+
+
+
+
 
